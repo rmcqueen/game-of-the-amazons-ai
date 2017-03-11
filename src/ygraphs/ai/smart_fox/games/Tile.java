e2413d3cf89e18add00a05a308129abac0237976
Ryan <kramer545@gmail.com>
2017-03-11 11:52:37 -0800

    added minDistHeur and tile java files

---
+++ src/ygraphs/ai/smart_fox/games/Tile.java
@@ -0,0 +1,12 @@
+package cosc332_server_test;
+
+public class Tile { public boolean isQueen;
+  
+  public Tile(int i, int j) { rowPos = i;
+    colPos = j;
+  }
+  
+  public boolean isOpponent;
+  int rowPos;
+  int colPos;
+}

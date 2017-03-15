68066480e6f97baad06078daf8a49435aa160b45
rcMcQueen <schroener1@gmail.com>
2017-03-15 13:18:32 -0700

    Added old/current position to Queen

--- src/Queen.java
+++ src/Queen.java
@@ -1,7 +1,9 @@
 package src;
 
 public class Queen extends Tile implements Cloneable{
-	
+	private int currentRow;
+    private int currentCol;
+
 	public Queen clone() throws CloneNotSupportedException{
         return (Queen) super.clone();
     }
@@ -10,8 +12,10 @@ public class Queen extends Tile implements Cloneable{
 
     /**
      *
-     * @param x: an integer storing the row location
-     * @param y: an integer storing the column location
+     * @param currentRow: an integer storing the row location
+     * @param currentCol: an integer storing the column location
+     * @param newRow: an integer storing the new Queen's row location
+     * @param newCol: an integer storing the new Queen's column location
      */
     public Queen(int x, int y) {
         super(x,y);
@@ -39,18 +43,25 @@ public class Queen extends Tile implements Cloneable{
         return super.col;
     }
 
+    public void setPreviousRowPosition(int row) {
+        this.currentRow = row;
+    }
+
+    public void setPreviousColPosition(int col) {
+        this.currentCol = col;
+    }
+
+    public int getPreviousRowPosition() {
+        return this.currentRow;
+    }
+
+    public int getPreviousColPosition() {
+        return this.currentCol;
+    }
+
     public void moveQueen(int row, int col) {
         super.row = row;
         super.col = col;
     }
-
-    /**
-     *
-     * @return: an Integer containing the row, and column combined.
-     */
-    public int getCombinedPosition() {
-        return Integer.parseInt(Integer.toString(super.row) + Integer.toString(super.col));
-    }
-
 
 }

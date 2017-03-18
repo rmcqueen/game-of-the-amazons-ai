eb42cee940ca7cc195df07bbf7ec6314f10ef781
rcMcQueen <schroener1@gmail.com>
2017-03-18 14:41:30 -0700

    Fixed placement of original piece positions

--- ygraphs/ai/smart_fox/games/Amazon.java
+++ ygraphs/ai/smart_fox/games/Amazon.java
@@ -267,14 +267,14 @@ public class Amazon extends GamePlayer{
 		public void init() {
             String tagB = BoardGameModel.POS_MARKED_BLACK;
             String tagW = BoardGameModel.POS_MARKED_WHITE;
-            gameModel.gameBoard[8][1] = tagW;
-            gameModel.gameBoard[8][10] = tagW;
+            gameModel.gameBoard[7][1] = tagW;
+            gameModel.gameBoard[7][10] = tagW;
             gameModel.gameBoard[10][4] = tagW;
             gameModel.gameBoard[10][7] = tagW;
             gameModel.gameBoard[1][4] = tagB;
             gameModel.gameBoard[1][7] = tagB;
-            gameModel.gameBoard[3][1] = tagB;
-            gameModel.gameBoard[3][10] = tagB;
+            gameModel.gameBoard[4][1] = tagB;
+            gameModel.gameBoard[4][10] = tagB;
 
         }
 		
